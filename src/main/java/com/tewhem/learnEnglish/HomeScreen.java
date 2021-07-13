23a9f5ca94fe96d56e53db659b3a744cb6ec94f0
mehmet-karagoz <atleownhem@gmail.com>
2021-07-13 18:00:25 +0300

    added HomeScreen

---
+++ src/main/java/com/tewhem/learnEnglish/HomeScreen.java
@@ -0,0 +1,19 @@
+package com.tewhem.learnEnglish;
+
+import java.awt.HeadlessException;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
+public class HomeScreen extends JFrame {
+
+    public HomeScreen(String title) throws HeadlessException {
+        super(title);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(new Rectangle(500, 200, 400, 300));
+        HomePanel panel = new HomePanel();
+        add(panel);
+    }
+
+}
